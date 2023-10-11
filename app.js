26b10ae621c01551fb37d6a3a7ee9975b5c7d3e7
Dimitri-Y <dimoon.bond@gmail.com>
2023-10-12 02:52:37 +0300

    start: check render

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const contactsRouter = require("./routes/api/contacts");
-const usersRouter = require("./routes/api/users");
 
 require("dotenv").config();
 
@@ -14,8 +12,6 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
-app.use("/api/users", usersRouter);
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
