3dd21f01781851e4dc6d889d7d8700cc0be9e740
Dimitri-Y <94981364+Dimitri-Y@users.noreply.github.com>
2023-10-13 22:35:02 +0300

    add logic server, swagger (#4)
    * add logic server, swagger
    
    * #bug/fix logic swagger

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 import app from "./app.js";
 import mongoose from "mongoose";
-
-const { DB_HOST, PORT } = process.env;
-mongoose.set("strictQuery", false);
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log("Database connection successful");
-    });
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+class Server {
+  constructor() {
+    this.app = app;
+  }
+  async start() {
+    const { DB_HOST, PORT } = process.env;
+    mongoose.set("strictQuery", false);
+    mongoose
+      .connect(DB_HOST)
+      .then(() => {
+        app.listen(PORT, () => {
+          console.log("Database connection successful");
+        });
+      })
+      .catch((error) => {
+        console.log(error.message);
+        process.exit(1);
+      });
+  }
+}
+new Server().start();
