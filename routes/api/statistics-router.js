e0d5cb32e4324a5ed6770fe0636aac7f2e227961
Dimitri-Y <dimoon.bond@gmail.com>
2023-10-15 21:47:00 +0300

    merge from main

---
+++ routes/api/statistics-router.js
@@ -0,0 +1,7 @@
+import express from "express";
+const statRouter = express.Router();
+statRouter.get("/");
+statRouter.post("/");
+statRouter.patch("/");
+statRouter.delete("/");
+export default statRouter;
