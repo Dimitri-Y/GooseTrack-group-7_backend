43d9b502931c8bf3c6182e60b89adf2eb1c04343
Alexander Klymuk <swcAlexander@gmail.com>
2023-10-17 20:19:37 +0300

    add changes

--- routes/api/tasks-router.js
+++ routes/api/tasks-router.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import taskController from '../../controllers/tasks-controller.js';
-// import authenticate from '../../middlewares/authenticate.js';
-import taskValidate from '../../middlewares/task-validation.js';
-import isValidId from '../../middlewares/isValidId.js';
+import {
+  authenticate,
+  taskValidate,
+  isValidId,
+} from '../../middlewares/index.js';
 
 const tasksRouter = express.Router();
 // tasksRouter.use(authenticate);
 
 tasksRouter.get('/', taskController.getAll);
 
-tasksRouter.post('/', taskValidate.addTaskValidate, taskController.add);
+tasksRouter.post('/', taskController.add);
+
 tasksRouter.patch(
   '/:Id',
   isValidId,
