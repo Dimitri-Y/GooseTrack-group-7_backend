22de6959fc77e727ab9a71154268b405264fd37d
Nikita1825 <nikitapostnov1825@gmail.com>
2023-10-18 12:08:26 +0300

    auth fix

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -4,6 +4,7 @@ const messageList = {
   403: 'Forbidden',
   404: 'Not Found',
   409: 'Conflict',
+  412: 'Precondition Failed',
 };
 
 const HttpError = (status, message = messageList[status]) => {
