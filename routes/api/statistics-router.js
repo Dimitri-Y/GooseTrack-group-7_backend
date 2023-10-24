62a60efe4653e613ed3840301c879e5c341658ef
Dimitri-Y <94981364+Dimitri-Y@users.noreply.github.com>
2023-10-25 00:09:29 +0300

    delete IsvalidId (#39)

--- routes/api/statistics-router.js
+++ routes/api/statistics-router.js
@@ -2,9 +2,8 @@ import express from "express";
 const statRouter = express.Router();
 import ctrl from "../../controllers/statistics-controller.js";
 import authenticate from "../../middlewares/authenticate.js";
-import { isValidIdDate } from "../../middlewares/isValidId.js";
 
 statRouter.get("/", authenticate, ctrl.getAllTasks);
-statRouter.get("/:date", authenticate, isValidIdDate, ctrl.getDayTasks);
+statRouter.get("/:date", authenticate, ctrl.getDayTasks);
 
 export default statRouter;
